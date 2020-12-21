51772c9a3d25f75bdc87de02b7200551d36a2271
Hadi Eskandari <hadieskandari01@gmail.com>
2020-12-22 02:34:45 +0330

    add usage.js for test

--- src/usage.js
+++ src/usage.js
@@ -14,11 +14,4 @@ var Complex = require('./func/complex').Complex;
 // console.log(newMatrix.showData());
 // let comp = complex(2, 3 , 'k');
 // console.log(comp.sentence);
-var comp = new Complex(complex(1, complex(2, 3, 'k'), 'k'));
-comp.unify();
-console.log(comp.unified.value);
-console.time('s');
-for (var i = 0; i < 1000; i++) {
-	console.log(comp.calculate(Math.log(i)));
-}
-console.timeEnd('s');
+
